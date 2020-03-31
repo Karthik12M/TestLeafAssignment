68182992b30f1d5a5b4cc122fa3133f31b7ce44d
Karthik12M <62940200+Karthik12M@users.noreply.github.com>
2020-03-31 17:53:05 +0530

    Add files via upload

---
+++ mobile.java
@@ -0,0 +1,29 @@
+package Week1.day1;
+
+public class mobile {
+
+	int a=98411;
+	String b="Karthik";
+	boolean c=true;
+	
+
+		public static void main(String[] args) {
+
+					
+		mobile newobjmoba = new mobile ();
+		int tempa = newobjmoba.a;
+		String tempb = newobjmoba.b;
+		boolean tempc = newobjmoba.c;
+		System.out.println(tempa);
+		System.out.println(tempb);
+		System.out.println(tempc);
+		
+		
+	    
+		
+		
+		
+		
+		
+}	
+}
