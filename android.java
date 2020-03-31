8b50d60814b03579f6528f97b0b2ab5198fd8ec0
Karthik12M <62940200+Karthik12M@users.noreply.github.com>
2020-03-31 19:42:47 +0530

    Add files via upload

---
+++ android.java
@@ -0,0 +1,28 @@
+package Week1.day1;
+
+public class android {
+	
+	int a=12;
+	float b=10.5f;
+	String c="Androidmobile";
+	boolean d=false;
+	long e=123456789;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		android tempobj = new android ();
+		int tempa = tempobj.a;
+		float tempb = tempobj.b;
+		String tempc = tempobj.c;
+		boolean tempd = tempobj.d;
+		long tempe = tempobj.e;
+		
+		System.out.println(tempa);
+		System.out.println(tempb);
+		System.out.println(tempc);
+		System.out.println(tempd);
+		System.out.println(tempe);
+	}
+
+}
